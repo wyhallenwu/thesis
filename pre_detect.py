1641f30c170184da8d10518e5e4385427929d75d
wyhallenwu <wyhforwork66@gmail.com>
2023-02-21 16:46:25 +0800

    pre-detection

--- pre_detect.py
+++ pre_detect.py
@@ -152,7 +152,8 @@ class Detector():
                     for item in result:
                         item.insert(0, file_size)
                         item.insert(0, f"{frame_num:06d}")
-                        f.write(' '.join([str(v) for v in item]), "\n")
+                        f.write(' '.join([str(v) for v in item]))
+                        f.write('\n')
 
 
 if __name__ == "__main__":
