65c432d44466f9f0007d46b56c6dd1d4fa16bcac
wyhallenwu <wyhforwork66@gmail.com>
2023-02-23 11:17:09 +0800

    adding gst

---
+++ gst_process.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+quantizer=(0 10 20 30 40 50)
+frame_rate=(30 20 15 10 5)
+
+python resize.py --frame_path=MOT16-04/img1 --saving_path=IMGS
+
+for q in ${quantizer[@]}
+do
+    for fr in ${frame_rate[@]}
+    do
+        gst-launch-1.0 multifilesrc location=MOT16-04/img1/%06d.jpg start-index=1 stop-index=1050 caps="image/jpeg,framerate=${fr}/1" ! decodebin ! videoconvert ! x264enc pass=5 speed-preset=1 quantizer=${q} tune=zerolatency threads=4 ! flvmux ! filesink location="test${q}_${fr}.flv"
+    done
+done
---
+++ resize.py
@@ -0,0 +1,32 @@
+import cv2
+import argparse
+import os
+from tqdm import tqdm
+
+resolution = [[1600, 900], [1280, 720], [960, 540], [640, 360]]
+
+
+def resize(path, rs, saving_path):
+    files = sorted(os.listdir(path))
+    for file in tqdm(files, desc="processing"):
+        frame_num = file[:-4]
+        frame = cv2.imread(path + '/' + file)
+        resized_frame = cv2.resize(frame, rs, interpolation=cv2.INTER_CUBIC)
+        cv2.imwrite(saving_path + '/' + frame_num + '.bpm', resized_frame)
+
+
+def main(frame_path, saving_path):
+    for rs in resolution:
+        if os.path.exists(f"{saving_path}/{rs[0]}_{rs[1]}"):
+            os.makedirs(f"{saving_path}/{rs[0]}_{rs[1]}")
+            print(f"making new folder {saving_path}/{rs[0]}_{rs[1]}")
+        resize(frame_path, rs, f"{saving_path}/{rs[0]}_{rs[1]}")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--frame_path", type=str, help="original frames")
+    parser.add_argument("--saving_path", type=str, help="saving result path")
+    args = parser.parse_args()
+    if args.frame_path and args.saving_path:
+        main(args.frame_path, args.saving_path)
