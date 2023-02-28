e2da73612245e1f97eae55ed768ac330d9439dc9
wyhallenwu <wyhforwork66@gmail.com>
2023-02-28 16:13:29 +0800

    fix gst process saving file

--- gst_process.sh
+++ gst_process.sh
@@ -5,6 +5,6 @@ for q in ${quantizer[@]}
 do
     for fr in ${frame_rate[@]}
     do
-        gst-launch-1.0 multifilesrc location=MOT16-04/img1/%06d.jpg start-index=1 stop-index=1050 caps="image/jpeg,framerate=${fr}/1" ! decodebin ! videoconvert ! x264enc pass=5 speed-preset=1 quantizer=${q} tune=zerolatency threads=4 ! flvmux ! filesink location="test${q}_${fr}.flv"
+        gst-launch-1.0 multifilesrc location=MOT16-04/img1/%06d.jpg start-index=1 stop-index=1050 caps="image/jpeg,framerate=${fr}/1" ! decodebin ! videoconvert ! x264enc pass=5 speed-preset=1 quantizer=${q} tune=zerolatency threads=8 ! flvmux ! filesink location="gst_videos/${q}_${fr}.flv"
     done
 done
