96ac2b5ca548aeb2054e58b5eede2f73d57282c6
wyhallenwu <wyhforwork66@gmail.com>
2023-02-28 16:38:14 +0800

    fix gst quan

--- gst_process.sh
+++ gst_process.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 quantizer=(5 15 25 35 45)
-# frame_rate=(30 20 15 10 5)
+frame_rate=(30 20 15 10 5)
 for q in ${quantizer[@]}
 do
-    gst-launch-1.0 multifilesrc location=MOT16-04/img1/%06d.jpg start-index=1 stop-index=1050 caps="image/jpeg,framerate=30/1" ! decodebin ! videoconvert ! x264enc pass=5 speed-preset=1 quantizer=${q} tune=zerolatency threads=8 ! flvmux ! filesink location="gst_videos/${q}.flv"
+    for fr in ${frame_rate[@]}
+    do
+        gst-launch-1.0 multifilesrc location=MOT16-04/img1/%06d.jpg start-index=1 stop-index=1050 caps="image/jpeg,framerate=${fr}/1" ! decodebin ! videoconvert ! x264enc pass=5 speed-preset=1 quantizer=${q} tune=zerolatency threads=8 ! flvmux ! filesink location="gst_videos/${q}_${fr}.flv"
+    done
 done
