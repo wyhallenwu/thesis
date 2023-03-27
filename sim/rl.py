e4681bee40d6d7f50bf338873add3466aad747be
wyhallenwu <wyhforwork66@gmail.com>
2023-03-27 16:23:25 +0800

    fix

--- sim/rl.py
+++ sim/rl.py
@@ -1,46 +1,36 @@
 import tianshou as ts
+from tianshou.utils.net.common import Net, ActorCritic
+from tianshou.utils.net.discrete import Actor, Critic
 from sim.env import SimEnv
 import torch
 import numpy as np
 from torch import nn
 
 
-class Net(nn.Module):
-    def __init__(self, state_shape, action_shape):
-        super().__init__()
-        self.model = nn.Sequential(
-            nn.Linear(np.prod(state_shape), 128), nn.ReLU(inplace=True),
-            nn.Linear(128, 128), nn.ReLU(inplace=True),
-            nn.Linear(128, 128), nn.ReLU(inplace=True),
-            nn.Linear(128, np.prod(action_shape)),
-        )
-
-    def forward(self, obs, state=None, info={}):
-        if not isinstance(obs, torch.Tensor):
-            obs = torch.tensor(obs, dtype=torch.float)
-        batch = obs.shape[0]
-        logits = self.model(obs.view(batch, -1))
-        return logits, state
-
-
 def run():
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
     train_env = SimEnv()
     test_env = SimEnv()
     state_shape = train_env.observation_space.shape
     action_shape = train_env.action_space.shape
-    net = Net(state_shape, action_shape)
-    optim = torch.optim.Adam(net.parameters(), lr=1e-3)
-    policy = ts.policy.DQNPolicy(
-        net, optim, 0.9, estimation_step=3, target_update_freq=100)
+    print(state_shape)
+    print(action_shape)
+    net = Net(state_shape, hidden_sizes=[64, 64], device=device)
+    actor = Actor(net, train_env.action_space.shape, device=device).to(device)
+    critic = Critic(net, device=device).to(device)
+    ac = ActorCritic(actor, critic)
+    optim = torch.optim.Adam(ac.parameters(), lr=1e-3)
+    dist = torch.distributions.Categorical
+    policy = ts.policy.PPOPolicy(
+        actor, critic, optim, dist, action_shape=action_shape)
     train_collector = ts.data.Collector(
-        policy, train_env, ts.data.VectorReplayBuffer(20000, 10), exploration_noise=True)
+        policy, train_env, ts.data.ReplayBuffer(200), exploration_noise=True)
     test_collector = ts.data.Collector(
         policy, test_env, exploration_noise=True)
-    result = ts.trainer.offpolicy_trainer(
+    result = ts.trainer.onpolicy_trainer(
         policy, train_collector, test_collector,
-        max_epoch=10, step_per_epoch=10000, step_per_collect=10,
-        update_per_step=0.1, episode_per_test=100, batch_size=64,
-        train_fn=lambda epoch, env_step: policy.set_eps(0.1),
-        test_fn=lambda epoch, env_step: policy.set_eps(0.05),
+        max_epoch=10, step_per_epoch=10000,
+        repeat_per_collect=10, episode_per_test=10,
+        batch_size=24, step_per_collect=100,
         stop_fn=lambda mean_rewards: mean_rewards >= 30)
     print(f'Finished training! Use {result["duration"]}')
